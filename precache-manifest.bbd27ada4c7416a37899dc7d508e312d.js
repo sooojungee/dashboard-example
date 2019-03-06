30edd616a48b21b3ed41ec8fe29571afb92dbf44
Soojung Lee <sooojungee@gmail.com>
2019-03-06 21:17:05 +0900

    Initial dist subtree commit

---
+++ precache-manifest.bbd27ada4c7416a37899dc7d508e312d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98236f05f0125567dfe8",
+    "url": "/dashboard/css/app.3717ae09.css"
+  },
+  {
+    "revision": "98236f05f0125567dfe8",
+    "url": "/dashboard/js/app.293ed649.js"
+  },
+  {
+    "revision": "5d3c6c23ba7b8b7e1e44",
+    "url": "/dashboard/css/chunk-vendors.01936c51.css"
+  },
+  {
+    "revision": "5d3c6c23ba7b8b7e1e44",
+    "url": "/dashboard/js/chunk-vendors.beae5e30.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/dashboard/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/dashboard/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c2e8094218ade6d4d748a8da32df7c9c",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dashboard/robots.txt"
+  }
+];
